2ea6e19fcc259bb6b40b73d5943be1a075322f0e
unknown <adarshkamble01@gmail.com>
2024-10-24 23:49:07 +0530

    popUp fix

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -11,8 +11,15 @@ import { Button } from "./ui/button";
 export function Dashboard() {
   const [walletNu, setWalletNu] = useState(0);
   const mnemonicsArr = useRecoilValue(seedAtom);
-  const [currWallet, setCurrWallet] = useState("");
+  const [currPubKey, setCurrPubKey] = useState("");
+  const [currPriKey, setCurrPriKey] = useState("");
   const [walletArr, setWalletArr] = useState<string[]>([]);
+  const [showPriKey, setShowPriKey] = useState(false);
+  const [showPriKeyTxt, setShowPriKeyTxt] = useState(false);
+
+  async function clickToCopy() {
+    await navigator.clipboard.writeText(currPriKey.toString());
+  }
 
   useEffect(() => {
     const mnemonics = mnemonicsArr.join(" ");
@@ -22,7 +29,8 @@ export function Dashboard() {
     const secret = nacl.sign.keyPair.fromSeed(derivedSeed).secretKey;
     // console.log(Buffer.from(secret).toString("base64"));
     // console.log(Keypair.fromSecretKey(secret).publicKey.toBase58());
-    setCurrWallet(Keypair.fromSecretKey(secret).publicKey.toBase58());
+    setCurrPubKey(Keypair.fromSecretKey(secret).publicKey.toBase58());
+    setCurrPriKey(Buffer.from(secret).toString("base64"));
     const newWallet = Keypair.fromSecretKey(secret).publicKey.toBase58();
     setWalletArr((prevWalletArr) => {
       // Only add if the wallet isn't already in the array to avoid duplicates
@@ -34,8 +42,12 @@ export function Dashboard() {
   }, [walletNu]);
 
   return (
-    <div>
-      <div className="bg-gradient-to-b from-neutral-800 from-5% to-black to-95% h-screen ">
+    <div className="relative h-screen ">
+      <div
+        className={`bg-gradient-to-b from-neutral-800 from-5% to-black to-95% h-screen absolute inset-0  ${
+          showPriKey ? "backdrop-blur-sm " : ""
+        }`}
+      >
         <div>
           <NavBar />
         </div>
@@ -53,12 +65,13 @@ export function Dashboard() {
             </div>
           </div>
           <div className="col-span-6 h-full bg-gradient-to-br from-neutral-800 from-25% to-black to-100%  mx-5  rounded-xl border border-zinc-700">
-            <div className="flex justify-center gap-5">
-              <div className="flex justify-center items-start mt-10 font-bold text-8xl text-white">
-                $0.00
-              </div>
-              <div className="mt-24 font-bold text-4xl text-stone-400">SOL</div>
+            <div className="mt-5 font-bold text-6xl text-stone-400 flex justify-center">
+              SOL
             </div>
+            <div className="flex justify-center items-start mt-2 font-bold text-5xl text-white">
+              $0.00
+            </div>
+
             <div className="flex justify-center mt-10 gap-5">
               <Button className="bg-white text-black gap-2 text-lg hover:bg-zinc-400">
                 Send
@@ -95,16 +108,160 @@ export function Dashboard() {
                 </svg>
               </Button>
             </div>
-            <div className="  border border-zinc-500 mt-10  mx-24"></div>
+            <div className="  w-full h-px   bg-gradient-to-r from-transparent via-zinc-500 to-transparent mt-10   "></div>
+            <div className="m-10">
+              <div className="bg-white w-max  px-5 h-max py-3 rounded-lg text-wrap">
+                <div className="font-bold text-lg">Sol</div>
+                <div className="font-semibold">Public Key: {currPubKey}</div>
+              </div>
+            </div>
           </div>
 
-          <div className="col-span-3 h-min bg-gradient-to-br from-neutral-800 from-25% to-black to-100%  border border-zinc-700 rounded-xl mr-36">
-            <div className="text-white p-5 font-semibold text-lg flex justify-center">
-              Add Wallets
+          <div className="col-span-3 h-max bg-gradient-to-br from-neutral-800 from-25% to-black to-100%  border border-zinc-700 rounded-xl mr-36">
+            <div className="">
+              <div className="flex  p-5 gap-3">
+                <div className="text-white font-semibold text-lg flex justify-center">
+                  Add Wallets
+                </div>
+                <div className="cursor-pointer ">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="white"
+                    className="size-7"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+              </div>
+              <div className="  w-full h-px   bg-gradient-to-r from-transparent via-zinc-500 to-transparent  "></div>
+              <div className="flex p-5 gap-3">
+                <div className="text-white font-semibold text-lg ">
+                  Private Key
+                </div>
+                <div
+                  className=""
+                  onClick={function () {
+                    setShowPriKey(true);
+                  }}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="white"
+                    className="size-7 cursor-pointer"
+                  >
+                    <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+                    <path
+                      fillRule="evenodd"
+                      d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
+      {showPriKey && (
+        <div className="flex justify-center items-center h-full absolute w-full select-none">
+          <div className=" bg-gradient-to-br from-neutral-800 from-55% to-black to-100% h-max w-max  py-10 px-16 rounded-xl border border-zinc-700">
+            <div className="flex justify-end">
+              <div className=" hover:rounded-full hover:bg-neutral-600 h-10 w-10  flex justify-center items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="white"
+                  className="size-8 cursor-pointer"
+                  onClick={function () {
+                    setShowPriKey(false);
+                    setShowPriKeyTxt(false);
+                  }}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18 18 6M6 6l12 12"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div className="text-white font-semibold text-lg ">
+              Private Key:
+            </div>
+            <div className="flex  mt-5 gap-5">
+              <div className=" bg-white h-max w-max p-2 rounded-lg">
+                <div className={showPriKeyTxt ? `font-semibold` : ``}>
+                  {showPriKeyTxt ? currPriKey : `●`.repeat(currPriKey.length)}{" "}
+                </div>
+              </div>
+              <div
+                className="flex items-center"
+                onClick={function (c) {
+                  setShowPriKeyTxt(!showPriKeyTxt);
+                }}
+              >
+                {!showPriKeyTxt && (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="white"
+                    className="size-6 cursor-pointer"
+                  >
+                    <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+                    <path
+                      fillRule="evenodd"
+                      d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                )}
+                {showPriKeyTxt && (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="white"
+                    className="size-6 cursor-pointer"
+                  >
+                    <path d="M3.53 2.47a.75.75 0 0 0-1.06 1.06l18 18a.75.75 0 1 0 1.06-1.06l-18-18ZM22.676 12.553a11.249 11.249 0 0 1-2.631 4.31l-3.099-3.099a5.25 5.25 0 0 0-6.71-6.71L7.759 4.577a11.217 11.217 0 0 1 4.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113Z" />
+                    <path d="M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0 1 15.75 12ZM12.53 15.713l-4.243-4.244a3.75 3.75 0 0 0 4.244 4.243Z" />
+                    <path d="M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 0 0-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 0 1 6.75 12Z" />
+                  </svg>
+                )}
+              </div>
+            </div>
+            <div className="flex justify-center mt-10 ">
+              <Button
+                onClick={clickToCopy}
+                className="flex gap-2 py-5 bg-white text-black font-bold hover:bg-zinc-400"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="size-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184"
+                  />
+                </svg>
+                Click to Copy
+              </Button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
